7d294e27efe54a919a712e3a80468bfb1e046e77
cagy <ml.cagy@seznam.cz>
2018-11-12 19:03:27 +0100

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Jan Čarnogurský on 12/11/2018.
+//  Copyright © 2018 Jan Čarnogurský. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
